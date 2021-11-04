3c1d48b13cc9b18516f69f9d7c91d9d8a23de393
yxs <asong0122@163.com>
2021-11-04 21:36:12 +0800

    用react脚手架实现hello word

---
+++ react_staging/src/App.js
@@ -0,0 +1,13 @@
+// 创建外壳组件App
+import React from "react"
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        hello, react
+      </div>
+    )
+  }
+}
+// 暴露app组件
+export default App
