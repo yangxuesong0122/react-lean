a86174759173cbcb11f65a1568a630286e6ef150
yxs <asong0122@163.com>
2021-11-04 22:05:15 +0800

    react hello初始化组件实现

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,13 +1,27 @@
 // 创建外壳组件App
-import React from "react"
-class App extends React.Component {
+// import React from "react"
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         hello, react
+//       </div>
+//     )
+//   }
+// }
+// // 暴露 App 组件
+// export default App
+
+import React, {Component} from "react"
+import Index from "./components/Hello"
+
+// 创建并暴露App组件
+export default class App extends Component {
   render() {
     return (
       <div>
-        hello, react
+        <Index/>
       </div>
     )
   }
 }
-// 暴露app组件
-export default App
---
+++ react_staging/src/components/Hello/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+import "./index.css"
+// 创建并暴露Hello组件
+export default class Index extends Component {
+  render() {
+    return (
+      <h2 className="bg-color">
+        hello, react,杨雪松啊
+      </h2>
+    )
+  }
+}
