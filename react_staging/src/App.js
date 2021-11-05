776a64f3adac8f320317a2a720443477f57b4384
yxs <y_xs@winning.com.cn>
2021-11-05 09:33:36 +0800

    实现todolist静态组件拆分

--- react_staging/src/App.js
+++ react_staging/src/App.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react'
+import './App.css'
+import Header from "./components/Header"
+import List from "./components/List"
+import Footer from "./components/Footer"
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App。。。
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header/>
+          <List/>
+          <Footer/>
+        </div>
       </div>
     )
   }
