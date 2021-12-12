185d74801abcae805f495cee28afd4601916b36c
yxs <asong0122@163.com>
2021-12-12 22:08:27 +0800

    react 向路由组件传递search参数

--- react_staging/15_向路由组件传递params参数/pages/Home/Message/index.jsx
+++ react_staging/15_向路由组件传递params参数/pages/Home/Message/index.jsx
@@ -20,10 +20,7 @@ export default class Message extends Component {
 							return (
 								<li key={msgObj.id}>
 									{/* 向路由组件传递params参数 */}
-									{/*<Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>*/}
-
-									{/* 向路由组件传递search参数 */}
-									<Link to={`/home/message/detail/?id=${msgObj.id}&title=${msgObj.title}`}>{msgObj.title}</Link>
+									<Link to={`/home/message/detail/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>
 								</li>
 							)
 						})
@@ -31,10 +28,7 @@ export default class Message extends Component {
 				</ul>
 				<hr/>
 				{/* 声明接收params参数 */}
-				{/*<Route path="/home/message/detail/:id/:title" component={Detail}/>*/}
-
-				{/* search参数无需声明接收 */}
-				<Route path="/home/message/detail" component={Detail}/>
+				<Route path="/home/message/detail/:id/:title" component={Detail}/>
 			</div>
 		)
 	}
--- react_staging/README.md
+++ react_staging/README.md
@@ -99,3 +99,8 @@
         路由链接(携带参数)：<Link to='/demo/test/tom/18'}>详情</Link>
         注册路由(声明接收)：<Route path="/demo/test/:name/:age" component={Test}/>
         接收参数：this.props.match.params
+    2.search参数
+        路由链接(携带参数)：<Link to='/demo/test?name=tom&age=18'}>详情</Link>
+        注册路由(无需声明，正常注册即可)：<Route path="/demo/test" component={Test}/>
+        接收参数：this.props.location.search
+        备注：获取到的search是urlencoded编码字符串，需要借助querystring解析
